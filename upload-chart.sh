9e8250b5221adc2c04e06b19ad8130a3b00920a0
Laurence MacGuire <leprechaun@gmail.com>
2023-08-15 13:08:05 +0700

    move package to folders

--- upload-chart.sh
+++ upload-chart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CHART_FILENAME="${1}"
+FOLDER_NAME="$($CHART_FILENAME | rev | cut -d'-' -f2- | rev)"
 
 CONTENT="$(cat $CHART_FILENAME | base64)"
 
@@ -13,5 +14,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/leprechaun/leprechaun.github.io/contents/${CHART_FILENAME} \
+  https://api.github.com/repos/leprechaun/leprechaun.github.io/contents/${FOLDER_NAME}/${CHART_FILENAME} \
   -d @payload.json
